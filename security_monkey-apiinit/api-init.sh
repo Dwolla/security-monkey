b9293f9fb8c4653481768393152853bc6e028d66
Skyler Nesheim <skyler@dwolla.com>
2017-03-13 15:33:19 -0500

    MASTER: add config file for api-init

--- security_monkey-apiinit/api-init.sh
+++ security_monkey-apiinit/api-init.sh
@@ -3,5 +3,7 @@
 mkdir -p /var/log/security_monkey/
 touch "/var/log/security_monkey/security_monkey-deploy.log"
 
+export SECURITY_MONKEY_SETTINGS=/usr/local/src/security_monkey/env-config/config-deploy.py
+
 cd /usr/local/src/security_monkey
 python manage.py db upgrade
