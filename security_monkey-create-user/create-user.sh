0f42f3c3f76141457ce36c20061412f61a2623bf
Skyler Nesheim <skyler@dwolla.com>
2017-03-16 16:17:54 -0500

    MASTER: use correct command when creating admin users

--- security_monkey-create-user/create-user.sh
+++ security_monkey-create-user/create-user.sh
@@ -6,4 +6,4 @@ touch "/var/log/security_monkey/security_monkey-deploy.log"
 export SECURITY_MONKEY_SETTINGS=/usr/local/src/security_monkey/env-config/config-deploy.py
 
 cd /usr/local/src/security_monkey
-python create_user.py $USER_EMAIL 'Admin'
+python manage.py create_user $USER_EMAIL 'Admin'
