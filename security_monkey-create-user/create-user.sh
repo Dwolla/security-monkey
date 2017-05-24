3740a53a3c0f1733cd2930ac60e466c1e8a9ee55
Skyler Nesheim <skyler@dwolla.com>
2017-05-24 16:21:11 -0500

    update to 0.9.1

--- security_monkey-create-user/create-user.sh
+++ security_monkey-create-user/create-user.sh
@@ -3,8 +3,8 @@
 mkdir -p /var/log/security_monkey/
 touch "/var/log/security_monkey/security_monkey-deploy.log"
 
-export SECURITY_MONKEY_SETTINGS=/usr/local/src/security_monkey/env-config/config-deploy.py
+export SECURITY_MONKEY_SETTINGS=/usr/local/src/security_monkey/env-config/config.py
 cd /usr/local/src/security_monkey
 
 echo "$(date +%x_%H:%M:%S:%N) - The create user service for security monkey is now starting..."
-python manage.py create_user $USER_EMAIL 'Admin'
+monkey create_user $USER_EMAIL 'Admin'
